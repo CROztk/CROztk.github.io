b22f6601b99cb831c53b1d94f9a49d5b507ec5d7
İrem <41iremcinar@gmail.com>
2025-02-04 16:49:39 +0100

    .

--- social/lib/Mobile/users_page_m.dart
+++ social/lib/Mobile/users_page_m.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:social/components/my_appbar.dart';
 import 'package:social/database/storage.dart';
@@ -12,70 +13,94 @@ class MyUsersPageMobile extends StatefulWidget {
 
 class _MyUsersPageMobileState extends State<MyUsersPageMobile> {
   final storage = Storage();
+  final currentUser = FirebaseAuth.instance.currentUser;
+
+  void _followUser(String userEmail) async {
+    // Check if user is already following
+    final userDoc =
+        FirebaseFirestore.instance.collection('users').doc(currentUser!.email);
+    final userSnapshot = await userDoc.get();
+
+    List<String> following = List<String>.from(userSnapshot['following'] ?? []);
+
+    if (!following.contains(userEmail)) {
+      // Add the user to the "following" list of the current user
+      following.add(userEmail);
+
+      // Update the current user's following list
+      await userDoc.update({
+        'following': following,
+      });
+
+      // Add the current user to the "followers" list of the followed user
+      final followedUserDoc =
+          FirebaseFirestore.instance.collection('users').doc(userEmail);
+      final followedUserSnapshot = await followedUserDoc.get();
+      List<String> followers =
+          List<String>.from(followedUserSnapshot['followers'] ?? []);
+      followers.add(currentUser!.email!);
+
+      await followedUserDoc.update({
+        'followers': followers,
+      });
+
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("You followed $userEmail")));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: MyAppbar(title: "s o C I a l"),
-        body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection("users").snapshots(),
-          builder: (context, snapshot) {
-            // loading
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            // error
-            if (snapshot.hasError) {
-              return Center(
-                child: Column(
-                  children: [
-                    const Icon(Icons.people_alt, size: 80),
-                    Text("Something went wrong"),
-                  ],
-                ),
-              );
-            }
-            // success
-            if (snapshot.hasData) {
-              // extract data
-              final users = snapshot.data!.docs;
+      appBar: MyAppbar(title: "s o C I a l"),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("users").snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
-              return ListView.builder(
-                itemCount: users.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(users[index]["username"]),
-                    subtitle: Text(users[index]["email"]),
-                    leading: FutureBuilder(
-                        future: storage.getImage(
-                            "profile_photos/", "${users[index]["email"]}.png"),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const CircularProgressIndicator();
-                          }
-                          if (snapshot.hasData) {
-                            return CircleAvatar(
-                              backgroundImage:
-                                  NetworkImage(snapshot.data.toString()),
-                            );
-                          }
-                          return const Icon(Icons.person, size: 40);
-                        }),
-                  );
-                },
-              );
-            }
-            // no data
-            return Center(
-              child: Column(
-                children: [
-                  const Icon(Icons.person, size: 80),
-                  Text("No users found"),
-                ],
-              ),
+          if (snapshot.hasError) {
+            return Center(child: Text("Something went wrong"));
+          }
+
+          if (snapshot.hasData) {
+            final users = snapshot.data!.docs;
+
+            return ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                final userEmail = users[index]["email"];
+                return ListTile(
+                  title: Text(users[index]["username"]),
+                  subtitle: Text(userEmail),
+                  leading: FutureBuilder(
+                    future:
+                        storage.getImage("profile_photos/", "${userEmail}.png"),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      }
+                      if (snapshot.hasData) {
+                        return CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(snapshot.data.toString()));
+                      }
+                      return const Icon(Icons.person, size: 40);
+                    },
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.follow_the_signs),
+                    onPressed: () => _followUser(userEmail),
+                  ),
+                );
+              },
             );
-          },
-        ));
+          }
+
+          return Center(child: Text("No users found"));
+        },
+      ),
+    );
   }
 }
--- social/lib/Tablet/profile_page_t.dart
+++ social/lib/Tablet/profile_page_t.dart
@@ -2,14 +2,26 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:social/components/my_appbar.dart';
+import 'package:social/database/firestore.dart';
+import 'package:social/database/storage.dart';
 
-class MyProfilePageTablet extends StatelessWidget {
-  MyProfilePageTablet({super.key});
+class MyProfilePageMobile extends StatefulWidget {
+  const MyProfilePageMobile({super.key});
 
-  // Current user
+  @override
+  State<MyProfilePageMobile> createState() => _MyProfilePageMobileState();
+}
+
+class _MyProfilePageMobileState extends State<MyProfilePageMobile> {
+  final storage = Storage();
   final currentUser = FirebaseAuth.instance.currentUser;
+  final FireStoreDatabase fireStore = FireStoreDatabase();
 
-  // Get details of the user
+  TextEditingController bioController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController dobController = TextEditingController();
+  bool isEditing = false;
+
   Future<DocumentSnapshot<Map<String, dynamic>>> getUserDetails() async {
     return await FirebaseFirestore.instance
         .collection('users')
@@ -17,66 +29,214 @@ class MyProfilePageTablet extends StatelessWidget {
         .get();
   }
 
+  void _changeProfilePicture() async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+
+    await storage.uploadImage("profile_photos/", "${currentUser!.email}");
+    Navigator.of(context).pop();
+    setState(() {});
+  }
+
+  void _saveProfile() async {
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(currentUser!.email)
+        .update({
+      'bio': bioController.text,
+      'username': nameController.text,
+      'dob': dobController.text,
+    });
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text("Profile updated successfully!")),
+    );
+
+    setState(() {
+      isEditing = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    bool isTablet = screenWidth > 600;
+
     return Scaffold(
-      appBar: MyAppbar(title: "s o C I a l"),
+      appBar: MyAppbar(title: "Profile"),
       body: FutureBuilder(
         future: getUserDetails(),
         builder: (context, snapshot) {
-          // Loading
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           }
-          // Error
           if (snapshot.hasError) {
             return Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(Icons.face_retouching_off, size: 80),
+              children: const [
+                Icon(Icons.error, size: 80),
                 Text("Something went wrong"),
               ],
             );
           }
-          // Success
           if (snapshot.hasData) {
-            // Extract data
             Map<String, dynamic>? user = snapshot.data!.data();
+            nameController.text = user?['username'] ?? '';
+            bioController.text = user?['bio'] ?? '';
+            dobController.text = user?['dob'] ?? '';
 
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // Adjusted position and size of the icon
-                  SizedBox(height: 40), // Adjusting the position of the icon
-                  const Icon(Icons.face, size: 100), // Enlarged icon
-
-                  // Adjusted text size
-                  SizedBox(height: 20), // Spacing between the icon and text
-                  Text(
-                    user!["username"],
-                    style: TextStyle(
-                        fontSize: 60,
-                        fontWeight: FontWeight.bold), // Enlarged text
+            return SingleChildScrollView(
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: isTablet ? screenWidth * 0.2 : 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          FutureBuilder(
+                            future: storage.getImage("profile_photos/",
+                                "${currentUser!.email!}.png"),
+                            builder: (context, snapshot) {
+                              if (snapshot.hasData) {
+                                return CircleAvatar(
+                                  radius: isTablet ? 80 : 45,
+                                  backgroundImage:
+                                      NetworkImage(snapshot.data.toString()),
+                                );
+                              }
+                              return const Icon(Icons.face, size: 80);
+                            },
+                          ),
+                          SizedBox(
+                            height: isTablet ? 140 : 110,
+                            width: isTablet ? 140 : 110,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                IconButton(
+                                  onPressed: _changeProfilePicture,
+                                  icon: const Icon(Icons.edit),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        user!["username"],
+                        style: TextStyle(
+                          fontSize: isTablet ? 36 : 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(user["email"]),
+                      Text(user["dob"] ?? "No birth date provided"),
+                      Text(user["bio"] ?? "No bio available"),
+                      const Divider(),
+                      if (!isEditing)
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              isEditing = true;
+                            });
+                          },
+                          child: const Text("Edit Profile"),
+                        ),
+                      if (isEditing)
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                controller: nameController,
+                                decoration: const InputDecoration(
+                                  labelText: 'Edit Name',
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              TextFormField(
+                                controller: dobController,
+                                decoration: const InputDecoration(
+                                  labelText: 'Edit Date of Birth',
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              TextFormField(
+                                controller: bioController,
+                                maxLines: 3,
+                                decoration: const InputDecoration(
+                                  labelText: 'Edit Bio',
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              ElevatedButton(
+                                onPressed: _saveProfile,
+                                child: const Text("Save Profile"),
+                              ),
+                            ],
+                          ),
+                        ),
+                      const Divider(),
+                      StreamBuilder(
+                        stream: fireStore.getPosts(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Center(
+                                child: CircularProgressIndicator());
+                          }
+                          if (snapshot.hasError) {
+                            return Column(
+                              children: const [
+                                Icon(Icons.error, size: 80),
+                                Text("Something went wrong"),
+                              ],
+                            );
+                          }
+                          if (snapshot.hasData) {
+                            final posts = snapshot.data!.docs;
+                            return ListView.builder(
+                              shrinkWrap: true,
+                              physics: NeverScrollableScrollPhysics(),
+                              itemCount: posts.length,
+                              itemBuilder: (context, index) {
+                                Timestamp timestamp = posts[index]["timestamp"];
+                                String username = posts[index]["username"];
+                                String message = posts[index]["message"];
+                                if (user["username"] != username) {
+                                  return const SizedBox();
+                                }
+                                return ListTile(
+                                  title: Text(message),
+                                  subtitle: Text(timestamp.toDate().toString()),
+                                );
+                              },
+                            );
+                          }
+                          return const Text("No data available");
+                        },
+                      )
+                    ],
                   ),
-                  Text(
-                    user["email"],
-                    style: TextStyle(fontSize: 30), // Enlarged email text
-                  ),
-                ],
+                ),
               ),
             );
           }
-
-          // No data
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(Icons.face_retouching_off, size: 80),
-              Text("No data"),
-            ],
-          );
+          return const Text("No data available");
         },
       ),
     );
